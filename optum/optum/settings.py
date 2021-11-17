30ac112e8443d258760daf2212925f6be7bd5eff
joyeetadey <joyeetadey99@gmail.com>
2021-11-18 04:42:56 +0530

    first commit

--- optum/optum/settings.py
+++ optum/optum/settings.py
@@ -40,9 +40,16 @@ INSTALLED_APPS = [
     'users',
 ]
 
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
+XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
